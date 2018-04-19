2f70149ef14f248191474bc88e2abbb4f8c8d0b9
eichmann <david-eichmann@uiowa.edu>
2018-04-19 21:35:55 +0000

    Added support to look up the application root in a property file for those cases where a vhost is in play or the proxy name differs from the container name.

--- src/main/java/edu/uiowa/tagUtil/request/ApplicationRoot.java
+++ src/main/java/edu/uiowa/tagUtil/request/ApplicationRoot.java
@@ -7,6 +7,7 @@
 package edu.uiowa.tagUtil.request;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
@@ -16,59 +17,84 @@ import javax.servlet.jsp.tagext.TagSupport;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import edu.uiowa.tagUtil.property.PropertyLoader;
+
 @SuppressWarnings("serial")
 public class ApplicationRoot extends TagSupport {
-	public static final Log log = LogFactory.getLog(ApplicationRoot.class);
-	
+    public static final Log log = LogFactory.getLog(ApplicationRoot.class);
+    static boolean propertyLoadAttempted = false;
+    static Properties properties = null;
+    static String container = null;
+    
     String theURI = null;
     String applicationRoot = null;
+    
+    private static synchronized void loadProperties(String applicationName) {
+	if (properties != null || propertyLoadAttempted)
+	    return;
+	propertyLoadAttempted = true;
+	log.info("loading " + applicationName + ".properties");
+	try {
+	    properties = PropertyLoader.loadProperties(applicationName + ".properties");
+	} catch (Exception e) {
+	    log.info("load failed: " + applicationName + ".properties");
+	}
+	if (properties != null) {
+	    container = properties.getProperty("applicationRoot", null);
+	    log.info("container: " + container);
+	}
+    }
 
     public int doStartTag() throws JspTagException {
-        // Note the misspelling of the actual header string value...
-        theURI = ((HttpServletRequest)pageContext.getRequest()).getRequestURI();
-        String thePath = ((HttpServletRequest)pageContext.getRequest()).getServletPath();
-        if (theURI.equals(thePath) || theURI.length() < 2)
-        	applicationRoot = "";
-        else
-        	applicationRoot = theURI.substring(0, theURI.indexOf('/', 1));
+	// Note the misspelling of the actual header string value...
+	theURI = ((HttpServletRequest) pageContext.getRequest()).getRequestURI();
+	String thePath = ((HttpServletRequest) pageContext.getRequest()).getServletPath();
+	if (theURI.equals(thePath) || theURI.length() < 2)
+	    applicationRoot = "";
+	else
+	    applicationRoot = theURI.substring(0, theURI.indexOf('/', 1));
+	loadProperties(applicationRoot.substring(1));
+	if (container != null)
+	    applicationRoot = container;
 
-        log.trace("theURI: " + theURI + "\tapplicationRoot:" + applicationRoot + "\t" + pageContext.getRequest().getServerName() + "\t" + pageContext.getRequest().getServerPort());
-        log.trace("servletPath: " + thePath);
-        try {
-            pageContext.getOut().print(applicationRoot);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            throw new JspTagException("Error generating application root.");
-        }
+	log.trace("theURI: " + theURI + "\tapplicationRoot:" + applicationRoot + "\t" + pageContext.getRequest().getServerName() + "\t"
+		+ pageContext.getRequest().getServerPort());
+	log.trace("servletPath: " + thePath);
+	try {
+	    pageContext.getOut().print(applicationRoot);
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	    throw new JspTagException("Error generating application root.");
+	}
 
-        return SKIP_BODY;
+	return SKIP_BODY;
     }
 
     public int doAfterBody() throws JspException {
-        clearServiceState();
-        return super.doEndTag();
+	clearServiceState();
+	return super.doEndTag();
     }
-    
+
     public String getTheURI() {
-        return theURI;
+	return theURI;
     }
 
     public void setTheURI(String theURI) {
-        this.theURI = theURI;
+	this.theURI = theURI;
     }
 
     public String getApplicationRoot() {
-        return applicationRoot;
+	return applicationRoot;
     }
 
     public void setApplicationRoot(String applicationRoot) {
-        this.applicationRoot = applicationRoot;
+	this.applicationRoot = applicationRoot;
     }
 
     private void clearServiceState() {
-        theURI = null;
-        applicationRoot = null;
+	theURI = null;
+	applicationRoot = null;
     }
 
 }
