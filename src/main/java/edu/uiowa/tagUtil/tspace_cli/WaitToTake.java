788fa4e938b86a4b3851dcf3c545e4b20381b8ab
eichmann <david-eichmann@uiowa.edu>
2021-06-10 22:06:01 +0000

    Switched to using log4j for logging.

--- src/main/java/edu/uiowa/tagUtil/tspace_cli/WaitToTake.java
+++ src/main/java/edu/uiowa/tagUtil/tspace_cli/WaitToTake.java
@@ -1,33 +1,36 @@
 package edu.uiowa.tagUtil.tspace_cli;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.ibm.tspaces.Field;
 import com.ibm.tspaces.Tuple;
 import com.ibm.tspaces.TupleSpace;
 import com.ibm.tspaces.TupleSpaceException;
 
 public class WaitToTake {
+    static Logger logger = Logger.getLogger(WaitToTake.class);
 	static TupleSpace ts = null;
 	static final String host = null;
 
 	public static void main(String[] args) throws TupleSpaceException {
-		System.out.println("host: " + args[0]);
-		System.out.println("tuple space: " + args[1]);
-		System.out.println("tuple: " + args[2] + (args.length > 3 ? "\tvalue: " + args[3] : ""));
+		PropertyConfigurator.configure(args[0]);
+		logger.info("waiting on: " + args[1] + " : " + args[2] + " : " + args[3] + (args.length > 4 ? "\tvalue: " + args[4] : ""));
 
-		ts = new TupleSpace(args[1], args[0]);
+		ts = new TupleSpace(args[2], args[1]);
 
-		if (args.length > 3) {
-				Tuple theTuple = ts.waitToTake(args[2] , new Field(String.class));
+		if (args.length > 4) {
+				Tuple theTuple = ts.waitToTake(args[3] , new Field(String.class));
 				if (theTuple != null) {
 					String value = (String) theTuple.getField(1).getValue();
-					System.out.println("consuming " + args[2]  + "\t" +  value);
+					logger.info("consuming " + args[3]  + "\t" +  value);
 				} else  {
 					
 				}
 		} else  {
-			Tuple theTuple = ts.waitToTake(args[2]);
+			Tuple theTuple = ts.waitToTake(args[3]);
 			if (theTuple != null) {
-				System.out.println("consuming " + args[2]);
+				logger.info("consuming " + args[3]);
 			} else  {
 				
 			}			
--- src/main/java/edu/uiowa/tagUtil/tspace_cli/Write.java
+++ src/main/java/edu/uiowa/tagUtil/tspace_cli/Write.java
@@ -1,23 +1,26 @@
 package edu.uiowa.tagUtil.tspace_cli;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.ibm.tspaces.TupleSpace;
 import com.ibm.tspaces.TupleSpaceException;
 
 public class Write {
+    static Logger logger = Logger.getLogger(Write.class);
 	static TupleSpace ts = null;
 	static final String host = null;
 
 	public static void main(String[] args) throws TupleSpaceException {
-		System.out.println("host: " + args[0]);
-		System.out.println("tuple space: " + args[1]);
-		System.out.println("tuple: " + args[2] + (args.length > 3 ? "\tvalue: " + args[3] : ""));
+		PropertyConfigurator.configure(args[0]);
+		logger.info("writing: " + args[1] + " : " + args[2] + " : " + args[3] + (args.length > 4 ? "\tvalue: " + args[4] : ""));
 
-		ts = new TupleSpace(args[1], args[0]);
+		ts = new TupleSpace(args[2], args[1]);
 
-		if (args.length > 3) {
-			ts.write(args[2], args[3]);
+		if (args.length > 4) {
+			ts.write(args[3], args[4]);
 		} else  {
-			ts.write(args[2]);
+			ts.write(args[3]);
 		}
 	}
 
