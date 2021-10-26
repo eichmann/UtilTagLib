49267a77e3738438ee5f424e8842bba33bfda341
eichmann <david-eichmann@uiowa.edu>
2021-10-26 14:08:39 +0000

    Pretty much functional CSV emitter.

--- src/main/java/edu/uiowa/tagUtil/csv/CSVEmitter.java
+++ src/main/java/edu/uiowa/tagUtil/csv/CSVEmitter.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Types;
 
 import javax.naming.InitialContext;
 import javax.servlet.jsp.JspException;
@@ -42,18 +43,40 @@ public class CSVEmitter extends TagSupport {
 				pageContext.getOut().print((first ? "" : ",") + name);
 				first = false;
 			}
+			pageContext.getOut().print("\n");
 			
 			PreparedStatement stmt = theConnection.prepareStatement("select * from " + schema + "." + table);
 			ResultSet rs = stmt.executeQuery();
+			ResultSetMetaData metars = rs.getMetaData();
 			while (rs.next()) {
-				ResultSetMetaData metars = rs.getMetaData();
+				for (int i = 1; i <= metars.getColumnCount(); i++) {
+					switch (metars.getColumnType(i)) {
+					case Types.INTEGER:
+					case Types.BIGINT:
+					case Types.SMALLINT:
+					case Types.TINYINT:
+						pageContext.getOut().print(rs.getInt(i));
+						break;
+					case Types.DOUBLE:
+						pageContext.getOut().print(rs.getDouble(i));
+						break;
+					case Types.FLOAT:
+						pageContext.getOut().print(rs.getFloat(i));
+						break;
+					default:
+						pageContext.getOut().print("\"" + rs.getString(i) + "\"");							
+					}
+					if (i < metars.getColumnCount())
+						pageContext.getOut().print(",");
+				}
+				pageContext.getOut().print("\n");
 			}
 		} catch (SQLException | IOException e) {
 			log.error("Error in database initialization", e);
 		}
 		return SKIP_PAGE;
 	}
-
+	
 	public int doEndTag() throws JspException {
 		clearServiceState();
 		return super.doEndTag();
