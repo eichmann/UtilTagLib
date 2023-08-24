490b2a20dd61ef10bd7f5d2ce8a4f6266d64dffd
eichmann <david-eichmann@uiowa.edu>
2023-08-24 16:08:30 +0000

    Tweaking handling of the applicationRoot tag to handle root-based apps better.

--- src/main/java/edu/uiowa/tagUtil/request/ApplicationRoot.java
+++ src/main/java/edu/uiowa/tagUtil/request/ApplicationRoot.java
@@ -12,6 +12,7 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.apache.logging.log4j.LogManager;
@@ -22,80 +23,88 @@ import edu.uiowa.tagUtil.property.PropertyLoader;
 @SuppressWarnings("serial")
 public class ApplicationRoot extends TagSupport {
 	static Logger logger = LogManager.getLogger(ApplicationRoot.class);
-    static boolean propertyLoadAttempted = false;
-    static Properties properties = null;
-    static String container = null;
-    
-    String theURI = null;
-    String applicationRoot = null;
-    
-    private static synchronized void loadProperties(String applicationName) {
-	if (properties != null || propertyLoadAttempted)
-	    return;
-	propertyLoadAttempted = true;
-	logger.info("loading " + applicationName + ".properties");
-	try {
-	    properties = PropertyLoader.loadProperties(applicationName + ".properties");
-	} catch (Exception e) {
-	    logger.info("load failed: " + applicationName + ".properties");
-	}
-	if (properties != null) {
-	    container = properties.getProperty("applicationRoot", null);
-	    logger.info("container: " + container);
-	}
-    }
+	static boolean propertyLoadAttempted = false;
+	static Properties properties = null;
+	static String container = null;
 
-    public int doStartTag() throws JspTagException {
-	// Note the misspelling of the actual header string value...
-	theURI = ((HttpServletRequest) pageContext.getRequest()).getRequestURI();
-	String thePath = ((HttpServletRequest) pageContext.getRequest()).getServletPath();
-	if (theURI.equals(thePath) || theURI.length() < 2)
-	    applicationRoot = "";
-	else {
-	    applicationRoot = theURI.substring(0, theURI.indexOf('/', 1));
-	    loadProperties(applicationRoot.substring(1));
-	}
-	if (container != null)
-	    applicationRoot = container;
+	String theURI = null;
+	String applicationRoot = null;
 
-	logger.trace("theURI: " + theURI + "\tapplicationRoot:" + applicationRoot + "\t" + pageContext.getRequest().getServerName() + "\t"
-		+ pageContext.getRequest().getServerPort());
-	logger.trace("servletPath: " + thePath);
-	try {
-	    pageContext.getOut().print(applicationRoot);
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	    throw new JspTagException("Error generating application root.");
+	private static synchronized void loadProperties(PageContext pageContext, String applicationName) {
+		if (properties != null || propertyLoadAttempted)
+			return;
+		propertyLoadAttempted = true;
+		logger.info("loading " + applicationName + ".properties");
+		try {
+			properties = PropertyLoader.loadProperties(applicationName + ".properties");
+		} catch (Exception e) {
+			logger.info("load failed: " + applicationName + ".properties");
+		}
+		if (properties != null) {
+			container = properties.getProperty("applicationRoot", null);
+			logger.info("container: " + container);
+		}
+		
+		if (container == null) {
+			logger.info("contextPath: " + ((HttpServletRequest) pageContext.getRequest()).getContextPath());
+			if (((HttpServletRequest) pageContext.getRequest()).getContextPath().equals("")) {
+				logger.info("setting container to ROOT");
+				container = "";
+			}
+		}
 	}
 
-	return SKIP_BODY;
-    }
+	public int doStartTag() throws JspTagException {
+		// Note the misspelling of the actual header string value...
+		theURI = ((HttpServletRequest) pageContext.getRequest()).getRequestURI();
+		String thePath = ((HttpServletRequest) pageContext.getRequest()).getServletPath();
+		if (theURI.equals(thePath) || theURI.length() < 2)
+			applicationRoot = "";
+		else {
+			applicationRoot = theURI.substring(0, theURI.indexOf('/', 1));
+			loadProperties(pageContext, applicationRoot.substring(1));
+		}
+		if (container != null)
+			applicationRoot = container;
 
-    public int doAfterBody() throws JspException {
-	clearServiceState();
-	return super.doEndTag();
-    }
+		logger.trace("theURI: " + theURI + "\tapplicationRoot:" + applicationRoot + "\t"
+				+ pageContext.getRequest().getServerName() + "\t" + pageContext.getRequest().getServerPort());
+		logger.trace("servletPath: " + thePath);
+		try {
+			pageContext.getOut().print(applicationRoot);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new JspTagException("Error generating application root.");
+		}
 
-    public String getTheURI() {
-	return theURI;
-    }
+		return SKIP_BODY;
+	}
 
-    public void setTheURI(String theURI) {
-	this.theURI = theURI;
-    }
+	public int doAfterBody() throws JspException {
+		clearServiceState();
+		return super.doEndTag();
+	}
 
-    public String getApplicationRoot() {
-	return applicationRoot;
-    }
+	public String getTheURI() {
+		return theURI;
+	}
 
-    public void setApplicationRoot(String applicationRoot) {
-	this.applicationRoot = applicationRoot;
-    }
+	public void setTheURI(String theURI) {
+		this.theURI = theURI;
+	}
 
-    private void clearServiceState() {
-	theURI = null;
-	applicationRoot = null;
-    }
+	public String getApplicationRoot() {
+		return applicationRoot;
+	}
+
+	public void setApplicationRoot(String applicationRoot) {
+		this.applicationRoot = applicationRoot;
+	}
+
+	private void clearServiceState() {
+		theURI = null;
+		applicationRoot = null;
+	}
 
 }
